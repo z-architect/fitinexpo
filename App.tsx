3ccdd374559ab094f6cbb1e43917de022186c404
Dawit Tadesse <davidog242@gmail.com>
2021-08-02 09:51:59 -0700

    all woman want to succed in life

---
+++ App.tsx
@@ -0,0 +1,30 @@
+import 'react-native-gesture-handler';
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+import useCachedResources from './hooks/useCachedResources';
+import useColorScheme from './hooks/useColorScheme';
+import Navigation from './navigation';
+import {NativeBaseProvider} from 'native-base';
+import FirstComp from './MyComponents/activityset';
+export default function App() {
+  const isLoadingComplete = useCachedResources();
+  const colorScheme = useColorScheme();
+
+  if (!isLoadingComplete) {
+    return null;
+  } else {
+    return (
+      <SafeAreaProvider>
+        <NativeBaseProvider>
+       <FirstComp/>
+       </NativeBaseProvider>
+      </SafeAreaProvider>
+    );
+  }
+}
+/*
+ <Navigation colorScheme={colorScheme} />
+        <StatusBar />
+        */
